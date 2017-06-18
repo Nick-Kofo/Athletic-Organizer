353fd70aafeb37e5a4fb3e21c734d97c59397b57
Nick <nikos.kofotolios@thanpa.com>
2017-06-18 19:04:01 +0300

    google auth still incoplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { AngularFireModule } from 'angularfire2';
+import firebase from 'firebase';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+export const firebaseConfig = {
+  apiKey: "AIzaSyDBDd1w0jNDRxW0CanVe2V_f2kgmBZV-So",
+  authDomain: "athletic-organizer.firebaseapp.com",
+  databaseURL: "https://athletic-organizer.firebaseio.com",
+  projectId: "athletic-organizer",
+  storageBucket: "athletic-organizer.appspot.com",
+  messagingSenderId: "256229658893"
+}
+firebase.initializeApp(firebaseConfig)
 
 @NgModule({
   declarations: [
@@ -14,7 +26,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +37,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    GooglePlus
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { AngularFireModule } from 'angularfire2';
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public googleplus:GooglePlus) {
 
   }
+
+  login() {
+    this.googleplus.login({
+      'webClientId': '256229658893-5petnodi446fp8bca3n3pk64pos61859.apps.googleusercontent.com',
+      'offline': true
+    }).then(res=>{
+        firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken))
+        .then(success=>{
+          alert('Login success')
+        }).catch(fail=>{
+          alert('Login failed')
+        })
+    }
+  
 
 }
