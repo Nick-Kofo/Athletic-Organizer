a20c5cc3abb0b0979d0a1508e3e6b7207c25a362
Nick <nikos.kofotolios@thanpa.com>
2017-07-09 11:19:38 +0300

    profile page change name and picture

--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -2,19 +2,30 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import firebase from 'firebase';
 
+/*
+  Generated class for the UserProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
 @Injectable()
 export class UserProvider {
-  firedata = firebase.database().ref('/chatusers');
+  firedata = firebase.database().ref('/users');
   constructor(public afireauth: AngularFireAuth) {
-    
   }
 
+  /*
+  Adds a new user to the system.
+  Called from - signup.ts
+  Inputs - The new user object containing the email, password and displayName.
+  Outputs - Promise.
+  */
   adduser(newuser) {
     var promise = new Promise((resolve, reject) => {
       this.afireauth.auth.createUserWithEmailAndPassword(newuser.email, newuser.password).then(() => {
         this.afireauth.auth.currentUser.updateProfile({
           displayName: newuser.displayName,
-          photoURL: ''
+          photoURL: 'https://firebasestorage.googleapis.com/v0/b/athletic-organizer.appspot.com/o/blank-profile-picture.png?alt=media&token=3177ba12-5794-4aa0-b3c3-edb8adc26349'
         }).then(() => {
           this.firedata.child(this.afireauth.auth.currentUser.uid).set({
             uid: this.afireauth.auth.currentUser.uid,
@@ -35,6 +46,12 @@ export class UserProvider {
     return promise;
   }
 
+  /*
+  For resetting the password of the user.
+  Called from - passwordreset.ts
+  Inputs - email of the user.
+  Output - Promise.
+  */
   passwordreset(email) {
     var promise = new Promise((resolve, reject) => {
       firebase.auth().sendPasswordResetEmail(email).then(() => {
@@ -46,6 +63,13 @@ export class UserProvider {
     return promise;
   }
 
+  /*
+  For updating the users collection and the firebase users list with
+  the imageurl of the profile picture stored in firebase storage.
+  Called from - profilepic.ts
+  Inputs - Url of the image stored in firebase.
+  OUtputs - Promise.
+  */
   updateimage(imageurl) {
       var promise = new Promise((resolve, reject) => {
           this.afireauth.auth.currentUser.updateProfile({
@@ -66,6 +90,39 @@ export class UserProvider {
              })  
       })
       return promise;
+  }
+
+  getuserdetails() {
+    var promise = new Promise((resolve, reject) => {
+    this.firedata.child(firebase.auth().currentUser.uid).once('value', (snapshot) => {
+      resolve(snapshot.val());
+    }).catch((err) => {
+      reject(err);
+      })
+    })
+    return promise;
+  }
+
+  updatedisplayname(newname) {
+    var promise = new Promise((resolve, reject) => {
+      this.afireauth.auth.currentUser.updateProfile({
+      displayName: newname,
+      photoURL: this.afireauth.auth.currentUser.photoURL
+    }).then(() => {
+      this.firedata.child(firebase.auth().currentUser.uid).update({
+        displayName: newname,
+        photoURL: this.afireauth.auth.currentUser.photoURL,
+        uid: this.afireauth.auth.currentUser.uid
+      }).then(() => {
+        resolve({ success: true });
+      }).catch((err) => {
+        reject(err);
+      })
+      }).catch((err) => {
+        reject(err);
+    })
+    })
+    return promise;
   }
 
 }
