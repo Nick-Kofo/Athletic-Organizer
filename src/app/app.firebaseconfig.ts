6476ea140f1669be37c15f11e9eb8903c56bfa05
Nick <nikos.kofotolios@thanpa.com>
2017-09-16 11:36:42 +0300

    refactor

--- src/app/app.firebaseconfig.ts
+++ src/app/app.firebaseconfig.ts
@@ -1,8 +1,8 @@
 export var config = {
-    apiKey: "AIzaSyDBDd1w0jNDRxW0CanVe2V_f2kgmBZV-So",
-    authDomain: "athletic-organizer.firebaseapp.com",
-    databaseURL: "https://athletic-organizer.firebaseio.com",
-    projectId: "athletic-organizer",
-    storageBucket: "gs://athletic-organizer.appspot.com",
+    apiKey: "*******************",
+    authDomain: "*******************",
+    databaseURL: "*******************",
+    projectId: "*******************",
+    storageBucket: "*******************",
     messagingSenderId: ""
 };
