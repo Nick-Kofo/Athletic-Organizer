c958daa9357f647eddbd1cf2c662e6ae3a6f9451
Nick <nikos.kofotolios@thanpa.com>
2017-11-25 10:18:19 +0200

    remove comments

--- src/providers/imghandler/imghandler.ts
+++ src/providers/imghandler/imghandler.ts
@@ -53,36 +53,6 @@ uploadimage() {
   return promise;
   }
 
-  // uploadimage() {
-  //   var promise = new Promise((resolve, reject) => {
-  //       this.filechooser.open().then((url) => {
-  //         (<any>window).FilePath.resolveNativePath(url, (result) => {
-  //           this.nativepath = result;
-  //           (<any>window).resolveLocalFileSystemURL(this.nativepath, (res) => {
-  //             res.file((resFile) => {
-  //               var reader = new FileReader();
-  //               reader.readAsArrayBuffer(resFile);
-  //               reader.onloadend = (evt: any) => {
-  //                 var imgBlob = new Blob([evt.target.result], { type: 'image/jpeg' });
-  //                 var imageStore = this.firestore.ref('/profileimages').child(firebase.auth().currentUser.uid);
-  //                 imageStore.put(imgBlob).then((res) => {
-  //                   this.firestore.ref('/profileimages').child(firebase.auth().currentUser.uid).getDownloadURL().then((url) => {
-  //                     resolve(url);
-  //                   }).catch((err) => {
-  //                       reject(err);
-  //                   })
-  //                 }).catch((err) => {
-  //                   reject(err);
-  //                 })
-  //               }
-  //             })
-  //           })
-  //         })
-  //     })
-  //   })    
-  //   return promise;   
-  // }
-
   picmsgstore() {
     var promise = new Promise((resolve, reject) => {
         this.filechooser.open().then((url) => {
